db21904f1bb429f6b1797164cdf5b444f9088e72
Joshua Hatzinger <joshuahatzinger@gmail.com>
2024-04-22 15:11:12 +0200

    Fusion of FUCaN with Better TUCaN

--- js/before_load.js
+++ js/before_load.js
@@ -6,4 +6,6 @@ var src = "$('#field_user').attr('placeholder', 'TU-ID');"+ //"$('#field_pass').
 var jq = document.createElement('script');
 jq.append(src);
 document.getElementsByTagName('head')[0].appendChild(jq);
+const navigation = document.getElementById("pageTopNavi");
+navigation.style.backgroundImage = 'url("' + chrome.runtime.getURL("img/Fucan_dark.png") + '")';
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+//PAGES
+const START = "Start"
+const VV = "Vorlesungsverzeichnis"
+const GRADES = "Notenspiegel"
+
+const PAGES = {
+  START: "PRGNAME=MLSSTART",
+  GRADES: "PRGNAME=GRADEOVERVIEW"
+};
+const SPECIAL_PAGES = {
+  VV: ["PRGNAME=ACTION", ]
+
+}
+console.log(PAGES.GRADES)
 function max(arr){
   return Math.max.apply(null, arr);
 }
@@ -52,7 +66,108 @@ alert(max());
 
 var url = window.location.href;
 
-if(url.includes('PRGNAME=GRADEOVERVIEW')){
+if(url.includes(PAGES.START)){
+
+  
+const header = document.getElementById("contentSpacer_IE").getElementsByTagName("h2")[1];
+
+
+//Aktuelles
+
+//TODO: Testen ob Seite Aktuelles ist
+const tableDiv = document.getElementsByClassName("tb rw-table")[0]
+const messageDiv = document.getElementsByClassName("tb rw-table")[1]
+
+const subheadSchedule = tableDiv.getElementsByClassName("tbsubhead");
+const subheadMessage = messageDiv.getElementsByClassName("tbsubhead");
+
+const scheduler = tableDiv.getElementsByClassName("tbcontrol")[0].getElementsByTagName("a")[0]
+const archive = messageDiv.getElementsByClassName("tbcontrol")[0].getElementsByTagName("a")[0]
+
+  let aktivitaeten = []
+  let nachrichtenList = []
+  
+  //Wenn table existiert
+  if (tableDiv.getElementsByTagName("table").length > 0) {
+     const veranstaltungen = tableDiv.getElementsByTagName("table")[0]
+     if (veranstaltungen.getElementsByTagName("tr").length > 1) {
+        for (i = 1; i < veranstaltungen.getElementsByTagName("tr").length; i++) {
+           let v = veranstaltungen.getElementsByTagName("tr")[i].cells[0].innerText
+           let name = veranstaltungen.getElementsByTagName("tr")[i].cells[1].innerText
+           let von_bis = "von " + veranstaltungen.getElementsByTagName("tr")[i].cells[2].innerText + " bis " + veranstaltungen.getElementsByTagName("tr")[i].cells[3].innerText
+           aktivitaeten.push(v + ";" + name + ";" + von_bis)
+        }
+     }
+  } else {
+     let text = "Es sind keine Aktivitäten für den Tag vorhanden!"
+     if (subheadMessage.length > 0) {
+        text = subheadSchedule[0].textContent
+     }
+     aktivitaeten.push(text + ";" + "" + ";" + "")
+  }
+  
+  aktivitaeten.push(scheduler.textContent + ";" + "<a href='" + scheduler.href + "' style='color: white'><span style='color: white'>" + '<i style="color: white;" class="fa-solid fa-clock"></i> ' + scheduler.textContent + "</a></span>" + ";" + "")
+  tableDiv.innerHTML = `<div class='parent'></div>`
+  const grid = document.getElementsByClassName("parent")[0];
+  const firstRow = document.createElement("div")
+  firstRow.classList = "elements"
+  firstRow.appendChild(header)
+  grid.appendChild(firstRow);
+  for (i = 0; i < aktivitaeten.length; i++) {
+     let sp = aktivitaeten[i].split(";");
+     let el = document.createElement("div")
+     el.classList = "elements"
+     el.innerHTML = "<h3>" + sp[0] + "</h3><h4>" + sp[1] + "</h4><h5>" + sp[2] + "</h5>"
+     grid.appendChild(el)
+  
+  }
+  
+  //Wenn message existiert
+  const msgHeader = messageDiv.getElementsByClassName("tbhead")[0]
+  if (messageDiv.getElementsByTagName("table").length > 0) {
+     const nachrichten = messageDiv.getElementsByTagName("table")[0]
+     if (nachrichten.getElementsByTagName("tr").length > 1) {
+        for (i = 1; i < nachrichten.getElementsByTagName("tr").length; i++) {
+           let datum = nachrichten.getElementsByTagName("tr")[i].cells[0].innerText
+           let uhrzeit = nachrichten.getElementsByTagName("tr")[i].cells[1].innerText
+           let vonan = nachrichten.getElementsByTagName("tr")[i].cells[2].innerText
+           let betreff = nachrichten.getElementsByTagName("tr")[i].cells[3].innerText
+           nachrichtenList.push(datum + ";" + uhrzeit + ";" + vonan + ";" + betreff)
+        }
+     }
+  } else {
+     let text = "Es sind keine Nachrichten vorhanden!"
+     if (subheadMessage.length > 0) {
+        text = subheadMessage[0].textContent
+     }
+     nachrichtenList.push(text + ";" + "" + ";" + "" + ";" + "")
+  }
+  
+  messageDiv.innerHTML = "<div class='parent'></div>"
+  nachrichtenList.push(archive.textContent + ";" + "<a href='" + archive.href + "' ><span style='color: white'>" + '<i style="color: white;" class="fa-solid fa-message"></i> ' + archive.textContent + "</a></span>" + ";" + "" + ";" + "")
+  
+  
+  
+  const grid2 = document.getElementsByClassName("parent")[1];
+  const firstRow2 = document.createElement("div")
+  firstRow2.classList = "elements"
+  const msgHeaderText = document.createElement("h2")
+  msgHeaderText.textContent = msgHeader.textContent
+  firstRow2.appendChild(msgHeaderText)
+  grid2.appendChild(firstRow2);
+  
+  for (i = 0; i < nachrichtenList.length; i++) {
+     let sp = nachrichtenList[i].split(";");
+     let el = document.createElement("div")
+     el.classList = "elements"
+     el.innerHTML = "<h3>" + sp[0] + "</h3><h4>" + sp[1] + "</h4><h5>" + sp[2] + "</h5><h5>" + sp[3] + "</h5>"
+     grid2.appendChild(el)
+  
+  }
+  //Remove
+
+}
+else if(url.includes(PAGES.GRADES)){
   //$('#pageContentPopUp > h2').eq(1).replaceWith('<h3>'+$('#pageContentPopUp > h2').eq(1).html()+'</h3>');
 
   labels = [];
